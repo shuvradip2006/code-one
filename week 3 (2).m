d0aafc04fbf12c50086d5a0710126306d1231604
shuvradip2006 <shuvradipsaharoy2006@gmail.com>
2025-09-23 12:38:19 +0530

    Add files via upload

---
+++ week 3 (2).m
@@ -0,0 +1,8 @@
+% Countdown from 10 to 1 using a while loop
+
+n = 10;  % Initialize the starting value
+
+while n >= 1
+    disp(n)      % Display the current value
+    n = n - 1;   % Decrement n by 1
+end
